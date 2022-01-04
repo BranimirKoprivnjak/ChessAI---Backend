a09cd5468832a0e334c24a94e1df1c917aaf1832
BranimirKoprivnjak <koprivnjak965@gmail.com>
2022-01-04 21:16:04 +0100

    'initial'

---
+++ app.py
@@ -0,0 +1,236 @@
+from flask import Flask, Response, render_template, request
+import chess
+import chess.svg
+import chess.engine
+import chess.polyglot
+import sys
+
+pawntable = [
+    0, 0, 0, 0, 0, 0, 0, 0,
+    5, 10, 10, -20, -20, 10, 10, 5,
+    5, -5, -10, 0, 0, -10, -5, 5,
+    0, 0, 0, 20, 20, 0, 0, 0,
+    5, 5, 10, 25, 25, 10, 5, 5,
+    10, 10, 20, 30, 30, 20, 10, 10,
+    50, 50, 50, 50, 50, 50, 50, 50,
+    0, 0, 0, 0, 0, 0, 0, 0]
+
+knightstable = [
+    -50, -40, -30, -30, -30, -30, -40, -50,
+    -40, -20, 0, 5, 5, 0, -20, -40,
+    -30, 5, 10, 15, 15, 10, 5, -30,
+    -30, 0, 15, 20, 20, 15, 0, -30,
+    -30, 5, 15, 20, 20, 15, 5, -30,
+    -30, 0, 10, 15, 15, 10, 0, -30,
+    -40, -20, 0, 0, 0, 0, -20, -40,
+    -50, -40, -30, -30, -30, -30, -40, -50]
+
+bishopstable = [
+    -20, -10, -10, -10, -10, -10, -10, -20,
+    -10, 5, 0, 0, 0, 0, 5, -10,
+    -10, 10, 10, 10, 10, 10, 10, -10,
+    -10, 0, 10, 10, 10, 10, 0, -10,
+    -10, 5, 5, 10, 10, 5, 5, -10,
+    -10, 0, 5, 10, 10, 5, 0, -10,
+    -10, 0, 0, 0, 0, 0, 0, -10,
+    -20, -10, -10, -10, -10, -10, -10, -20]
+
+rookstable = [
+    0, 0, 0, 5, 5, 0, 0, 0,
+    -5, 0, 0, 0, 0, 0, 0, -5,
+    -5, 0, 0, 0, 0, 0, 0, -5,
+    -5, 0, 0, 0, 0, 0, 0, -5,
+    -5, 0, 0, 0, 0, 0, 0, -5,
+    -5, 0, 0, 0, 0, 0, 0, -5,
+    5, 10, 10, 10, 10, 10, 10, 5,
+    0, 0, 0, 0, 0, 0, 0, 0]
+
+queenstable = [
+    -20, -10, -10, -5, -5, -10, -10, -20,
+    -10, 0, 0, 0, 0, 0, 0, -10,
+    -10, 5, 5, 5, 5, 5, 0, -10,
+    0, 0, 5, 5, 5, 5, 0, -5,
+    -5, 0, 5, 5, 5, 5, 0, -5,
+    -10, 0, 5, 5, 5, 5, 0, -10,
+    -10, 0, 0, 0, 0, 0, 0, -10,
+    -20, -10, -10, -5, -5, -10, -10, -20]
+
+kingstable = [
+    20, 30, 10, 0, 0, 10, 30, 20,
+    20, 20, 0, 0, 0, 0, 20, 20,
+    -10, -20, -20, -20, -20, -20, -20, -10,
+    -20, -30, -30, -40, -40, -30, -30, -20,
+    -30, -40, -40, -50, -50, -40, -40, -30,
+    -30, -40, -40, -50, -50, -40, -40, -30,
+    -30, -40, -40, -50, -50, -40, -40, -30,
+    -30, -40, -40, -50, -50, -40, -40, -30]
+
+def evaluate_board():
+    if board.is_checkmate():
+        if board.turn:
+            return -9999
+        else:
+            return 9999
+    if board.is_stalemate():
+        return 0
+    if board.is_insufficient_material():
+        return 0
+
+    # returns number of pieces on board
+    wp = len(board.pieces(chess.PAWN, chess.WHITE))
+    bp = len(board.pieces(chess.PAWN, chess.BLACK))
+    wn = len(board.pieces(chess.KNIGHT, chess.WHITE))
+    bn = len(board.pieces(chess.KNIGHT, chess.BLACK))
+    wb = len(board.pieces(chess.BISHOP, chess.WHITE))
+    bb = len(board.pieces(chess.BISHOP, chess.BLACK))
+    wr = len(board.pieces(chess.ROOK, chess.WHITE))
+    br = len(board.pieces(chess.ROOK, chess.BLACK))
+    wq = len(board.pieces(chess.QUEEN, chess.WHITE))
+    bq = len(board.pieces(chess.QUEEN, chess.BLACK))
+
+    material = 100 * (wp - bp) + 320 * (wn - bn) + 330 * (wb - bb) + 500 * (wr - br) + 900 * (wq - bq)
+
+    pawnsq = sum([pawntable[i] for i in board.pieces(chess.PAWN, chess.WHITE)])
+    pawnsq = pawnsq + sum([-pawntable[chess.square_mirror(i)]
+                           for i in board.pieces(chess.PAWN, chess.BLACK)])
+    knightsq = sum([knightstable[i] for i in board.pieces(chess.KNIGHT, chess.WHITE)])
+    knightsq = knightsq + sum([-knightstable[chess.square_mirror(i)]
+                               for i in board.pieces(chess.KNIGHT, chess.BLACK)])
+    bishopsq = sum([bishopstable[i] for i in board.pieces(chess.BISHOP, chess.WHITE)])
+    bishopsq = bishopsq + sum([-bishopstable[chess.square_mirror(i)]
+                               for i in board.pieces(chess.BISHOP, chess.BLACK)])
+    rooksq = sum([rookstable[i] for i in board.pieces(chess.ROOK, chess.WHITE)])
+    rooksq = rooksq + sum([-rookstable[chess.square_mirror(i)]
+                           for i in board.pieces(chess.ROOK, chess.BLACK)])
+    queensq = sum([queenstable[i] for i in board.pieces(chess.QUEEN, chess.WHITE)])
+    queensq = queensq + sum([-queenstable[chess.square_mirror(i)]
+                             for i in board.pieces(chess.QUEEN, chess.BLACK)])
+    kingsq = sum([kingstable[i] for i in board.pieces(chess.KING, chess.WHITE)])
+    kingsq = kingsq + sum([-kingstable[chess.square_mirror(i)]
+                           for i in board.pieces(chess.KING, chess.BLACK)])
+
+    eval = material + pawnsq + knightsq + bishopsq + rooksq + queensq + kingsq
+    #change heuristic value based on players turn
+    if board.turn:
+        return eval
+    else:
+        return -eval
+
+def alphabeta(alpha, beta, depthleft):
+    bestscore = -9999
+    if (depthleft == 0):
+        return evaluate_board()
+    for move in board.legal_moves:
+        board.push(move)
+        score = -alphabeta(-beta, -alpha, depthleft - 1)
+        board.pop()
+        if (score >= beta):
+            return score
+        if (score > bestscore):
+            bestscore = score
+        if (score > alpha):
+            alpha = score
+    return bestscore
+
+def selectmove(depth):
+    try:
+        move = chess.polyglot.MemoryMappedReader('books/human.bin').weighted_choice(board).move
+        return move;
+    except:
+        bestMove = chess.Move.null()
+        bestValue = -99999
+        alpha = -100000
+        beta = 100000
+        for move in board.legal_moves:
+            board.push(move)
+            boardValue = -alphabeta(-beta, -alpha, depth-1)
+            if boardValue > bestValue:
+                bestValue = boardValue
+                bestMove = move
+            if( boardValue > alpha ):
+                alpha = boardValue
+            board.pop()
+        return bestMove
+
+def minimax():
+    move = selectmove(depth)
+    board.push(move)
+    #return main()
+
+def stockfish():
+    if (sys.platform == 'win32'):
+        engine = chess.engine.SimpleEngine.popen_uci(
+            'engines/stockfish_14.1_win_x64_avx2/stockfish_14.1_win_x64_avx2.exe'
+        )
+    else:
+        engine = chess.engine.SimpleEngine.popen_uci(
+            'engines/stockfish_14.1_linux_x64_avx2/stockfish_14.1_linux_x64_avx2.exe'
+        )
+    move = engine.play(board, chess.engine.Limit(time=0.2))
+    board.push(move.move)
+
+app = Flask(__name__)
+
+#cant be same name as routing function
+board = chess.Board()
+#search tree depth
+depth = 3
+
+@app.route('/board')
+def chess_board():
+    return Response(chess.svg.board(board=board, size=700), mimetype='image/svg+xml')
+
+#render board
+@app.route('/')
+def main(message=''):
+    if board.is_check():
+        message = 'Check!'
+    if board.is_checkmate():
+        message = 'Checkmate!'
+    if board.is_stalemate():
+        message = 'Draw by stalemate!'
+    if board.is_insufficient_material():
+        message = 'Draw by insufficient material!'
+    return render_template('index.html', message=message)
+
+#human move
+@app.route('/move/')
+def move():
+    move = request.args.get('move', default="")
+    # 1st checks if input is in right format, if not, raises ValueError
+    # 2nd it checks if move is legal
+    message = ''
+    try:
+        if chess.Move.from_uci(move) in board.legal_moves:
+            board.push_san(move)
+            minimax()
+    except ValueError:
+        message = 'Invalid move format!'
+    return main(message)
+
+#stockfish move
+@app.route('/engine/')
+def engine():
+    message = ''
+    try:
+        stockfish()
+        minimax()
+    except Exception:
+        message='Illegal move, try again!'
+    return main(message)
+
+#new game
+@app.route('/game/')
+def game():
+    board.reset()
+    return main()
+
+#undo last move
+@app.route('/undo/')
+def undo():
+    message = ''
+    try:
+        board.pop()
+    except Exception:
+        message='Nothing to undo!'
+    return main(message)
